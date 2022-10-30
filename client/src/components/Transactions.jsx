4fa2631f3efe773734160901c167c68fbdda9d60
minhluan.vo <vominhluan0406@gmail.com>
2022-10-30 10:20:51 +0700

    do: add smart_contract

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,13 @@
 const Transactions = () => () => {
   return (
-    <h1>Footer</h1>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        
+          <h3 className="text-white text-3xl text-center my-2">
+            Connect your account to see the latest transactions
+          </h3>
+      </div>
+    </div>
   )
 }
 
